6ac6672c82b0d28739245a718fb74ddae947b8bc
Ramy Sabry <ramysabry1996@gmail.com>
2021-09-24 18:29:06 +0200

    Initial Commit

---
+++ Fructus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Ramy Sabry on 24/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
